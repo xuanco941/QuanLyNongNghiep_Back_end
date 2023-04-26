8e4dc4bc24e4354cf05bc85a6eb7fc0b3132c48b
xuanco941 <xuanco941@gmail.com>
2023-04-26 11:24:51 +0700

    change db

---
+++ QuanLyNongNghiepAPI/Migrations/20230426041610_v4.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuanLyNongNghiepAPI.Migrations
+{
+    public partial class v4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Avatar",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "User");
+        }
+    }
+}
